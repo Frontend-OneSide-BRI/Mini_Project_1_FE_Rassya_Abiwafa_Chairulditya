f614b2acb45bba06c25efc8d6fb519770b4196ef
Rassya Abiwafa Chairulditya <rassya.abiwafa@gmail.com>
2023-05-31 11:28:33 +0700

    add and complete filter function to all three tabs

--- assets/script/script.js
+++ assets/script/script.js
@@ -19,6 +19,26 @@ const natureImage = [
             url : "https://images.unsplash.com/photo-1473496169904-658ba7c44d8a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
             tag : "nature"
         },
+        {
+            id: 5,
+            url : "https://images.unsplash.com/photo-1683009427619-a1a11b799e05?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+            tag : "nature"
+        },
+        {
+            id: 6,
+            url : "https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=874&q=80",
+            tag : "nature"
+        },
+        {
+            id: 7,
+            url : "https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+            tag : "nature"
+        },
+        {
+            id: 8,
+            url : "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+            tag : "nature"
+        },
 ]
 
 const gadgetImage = [
@@ -42,17 +62,62 @@ const gadgetImage = [
         url : "https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80",
         tag : "gadget"
     },
+    {
+        id: 5,
+        url : "https://images.unsplash.com/photo-1595087873528-1fe582a3b302?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80",
+        tag : "gadget"
+    },
+    {
+        id: 6,
+        url : "https://images.unsplash.com/photo-1602526432604-029a709e131c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        tag : "gadget"
+    },
+    {
+        id: 7,
+        url : "https://images.unsplash.com/photo-1542751110-97427bbecf20?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=784&q=80",
+        tag : "gadget"
+    },
+    {
+        id: 8,
+        url : "https://images.unsplash.com/photo-1558089687-f282ffcbc126?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80",
+        tag : "gadget"
+    },
+]
+
+const musicImage = [
+    {
+        id: 1,
+        url : "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80&1",
+        tag : "music"
+    },
+    {
+        id: 2,
+        url : "https://images.unsplash.com/photo-1507838153414-b4b713384a76?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        tag : "music"
+    },
+    {
+        id: 3,
+        url : "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        tag : "music"
+    },
+    {
+        id: 4,
+        url : "https://images.unsplash.com/photo-1506157786151-b8491531f063?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        tag : "music"
+    },
 ]
 
 // Get elemen from HTML
 const nature = document.getElementById("nature");
+const gadget = document.getElementById('gadget');
+const music = document.getElementById('music')
 
 const galleryWrapper = document.getElementById('gallery-wrapper')
 
 
 // function
-function filterNature(arr){
-    galleryWrapper.textContent=" "
+function filterImage(arr){
+    galleryWrapper.textContent=" ";
     for(let i = 0; i < arr.length; i++){
         // create card image and add class
         // Create element
@@ -68,11 +133,17 @@ function filterNature(arr){
 
         // append card image to gallery wrapper
         galleryWrapper.appendChild(cardImage)
-
-        console.log(i)
     }
 }
 
 nature.addEventListener('click', function(){
-    filterNature(natureImage)
+    filterImage(natureImage)
+})
+
+gadget.addEventListener('click', function(){
+    filterImage(gadgetImage)
+})
+
+music.addEventListener('click', function(){
+    filterImage(musicImage)
 })
