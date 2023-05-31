d21edcab0d108ae00941271ea04baf90e057e1df
Rassya Abiwafa Chairulditya <rassya.abiwafa@gmail.com>
2023-05-31 21:42:54 +0700

    update homepage

--- assets/script/script.js
+++ assets/script/script.js
@@ -127,15 +127,43 @@ const musicImage = [
     },
 ]
 
+const bannerImage =[
+    {
+        id:1,
+        url:'https://source.unsplash.com/collection/928423/480x480&1',
+        tag : 'bannerImage1'
+    },
+    {
+        id : 2,
+        url: 'https://source.unsplash.com/collection/928423/480x480&2',
+        tag: 'bannerImage2'
+    },
+    {
+        id : 3,
+        url : 'https://source.unsplash.com/collection/928423/480x480&3',
+        tag: 'bannerImage3'
+    }
+]
+
 const images = [
     {
-        id: 1,
-        url : "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=873&q=80",
-        tag : "nature"
+        id: 6,
+        url : "https://plus.unsplash.com/premium_photo-1682501735244-cc1343382b04?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=869&q=80",
+        tag : "music"
+    },
+    {
+        id: 7,
+        url : "https://images.unsplash.com/photo-1501612780327-45045538702b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        tag : "music"
     },
     {
         id: 2,
         url : "https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=872&q=80",
+        tag : "nature"
+    },
+    {
+        id: 1,
+        url : "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=873&q=80",
         tag : "nature"
     },
     {
@@ -149,13 +177,13 @@ const images = [
         tag : "nature"
     },
     {
-        id: 5,
-        url : "https://images.unsplash.com/photo-1683009427619-a1a11b799e05?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        id: 6,
+        url : "https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=874&q=80",
         tag : "nature"
     },
     {
-        id: 6,
-        url : "https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=874&q=80",
+        id: 5,
+        url : "https://images.unsplash.com/photo-1683009427619-a1a11b799e05?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
         tag : "nature"
     },
     {
@@ -189,26 +217,6 @@ const images = [
         tag : "gadget"
     },
     {
-        id: 5,
-        url : "https://images.unsplash.com/photo-1595087873528-1fe582a3b302?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80",
-        tag : "gadget"
-    },
-    {
-        id: 6,
-        url : "https://images.unsplash.com/photo-1602526432604-029a709e131c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
-        tag : "gadget"
-    },
-    {
-        id: 7,
-        url : "https://images.unsplash.com/photo-1542751110-97427bbecf20?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=784&q=80",
-        tag : "gadget"
-    },
-    {
-        id: 8,
-        url : "https://images.unsplash.com/photo-1558089687-f282ffcbc126?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80",
-        tag : "gadget"
-    },
-    {
         id: 1,
         url : "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80&1",
         tag : "music"
@@ -230,17 +238,27 @@ const images = [
     },
     {
         id: 5,
-        url : "https://images.unsplash.com/photo-1484876065684-b683cf17d276?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
-        tag : "music"
+        url : "https://images.unsplash.com/photo-1595087873528-1fe582a3b302?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80",
+        tag : "gadget"
     },
     {
         id: 6,
-        url : "https://plus.unsplash.com/premium_photo-1682501735244-cc1343382b04?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=869&q=80",
-        tag : "music"
+        url : "https://images.unsplash.com/photo-1602526432604-029a709e131c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        tag : "gadget"
     },
     {
         id: 7,
-        url : "https://images.unsplash.com/photo-1501612780327-45045538702b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+        url : "https://images.unsplash.com/photo-1542751110-97427bbecf20?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=784&q=80",
+        tag : "gadget"
+    },
+    {
+        id: 8,
+        url : "https://images.unsplash.com/photo-1558089687-f282ffcbc126?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80",
+        tag : "gadget"
+    },
+    {
+        id: 5,
+        url : "https://images.unsplash.com/photo-1484876065684-b683cf17d276?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
         tag : "music"
     },
     {
@@ -256,18 +274,17 @@ const nature = document.getElementById("nature");
 const gadget = document.getElementById('gadget');
 const music = document.getElementById('music');
 const all = document.getElementById('allImage');
-
+const search = document.getElementById('search')
 const galleryWrapper = document.getElementById('gallery-wrapper')
+const imageCategory1 = document.getElementById('image-category-1')
 
-// Place all images
-filterImage(images)
 
 // function to filter image
 function filterImage(arr){
     galleryWrapper.textContent=" ";
     for(let i = 0; i < arr.length; i++){
-        // let randomNumber = Math.ceil(Math.random() * arr.length)
         // create card image and add class
+
         // Create element
         const cardImage = document.createElement('div')
         cardImage.classList.add('col-12', 'col-md-4',  'col-lg-3', 'card-image')
@@ -281,9 +298,36 @@ function filterImage(arr){
 
         // append card image to gallery wrapper
         galleryWrapper.appendChild(cardImage)
+
+        galleryWrapper.scrollIntoView()
     }
 }
 
+// function show image by tag
+function showItemsByTag(tag) {
+    const filteredImages = images.filter(image => image.tag.includes(tag));
+
+    console.log(filteredImages)
+  
+    if(filteredImages.length > 1){
+        filterImage(filteredImages)
+    } else {
+        galleryWrapper.textContent=" ";
+        // Create element
+        const h1 = document.createElement('h1')
+        h1.classList.add("my-5", "fs-5")
+        h1.innerText="~ gambar tidak ditemukan ~"
+
+        galleryWrapper.appendChild(h1)
+
+        galleryWrapper.scrollIntoView()
+    }
+}
+
+// function place all images
+filterImage(images)
+
+// Add Event
 nature.addEventListener('click', function(){
     filterImage(natureImage)
 })
@@ -298,4 +342,11 @@ music.addEventListener('click', function(){
 
 all.addEventListener('click', function(){
     filterImage(images)
+})
+
+search.addEventListener('keypress', function(e){
+    let input = e.target.value
+    if(e.key === "Enter"){
+        showItemsByTag(input)
+    }
 })
